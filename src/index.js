b4a25e78234c199ef71b39b4daf586cdba043b85
Amsterdam <james.s@mmtdigital.co.uk>
2018-02-05 10:21:33 +0000

    Test commit undo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import '@styles/styles.scss'
 // You can either write your JS in this file, or organise your
 // own project and import/export modules using ES6 modules.
 
-import trump from '@images/trump.jpg' //a
+import trump from '@images/trump.jpg'

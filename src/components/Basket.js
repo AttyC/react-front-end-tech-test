75917bdc8bac4ce80bb33eb397ce705ee8e824e5
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-24 20:26:57 +0000

    items price * quantity displayed

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,10 +3,17 @@ import Item from './Item';
 
 class Basket extends React.Component {
   render() {
+    const itemList = [
+      { id: 1, name: "Mountain Dew", price: "1.80"},
+      { id: 2, name: "Desperados", price: "2.60"},
+      { id: 3, name: "Jack Daniels", price: "3.35"}
+    ];
+
     return (
       <div className="basket">
-        <Item name="Mountain Dew" price="1.80"/>
-        <Item name="Jack Daniels" price="3.35"/>
+        {itemList.map( item =>
+          <Item name={item.name} price={item.price}/>
+        )}
       </div>
     )
   }

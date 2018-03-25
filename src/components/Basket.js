5524e8cec8eb097746d0e518d4e17463c9b7ef8c
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-25 17:06:05 +0100

    update Item class to calculate price based on quantity

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,41 +3,19 @@ import Item from './Item';
 
 class Basket extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      quantity: 0
-    };
-
-    this.handleUpdateQuantity = this.handleUpdateQuantity.bind(this);
-  }
-
-  handleUpdateQuantity(event){
-    this.setState({
-      quantity: Number(event.target.value)
-    });
-  }
   render() {
     const itemList = [
-      { id: 1, name: "Mountain Dew", price: "1.80", quantity: this.state.quantity },
-      { id: 2, name: "Desperados", price: "2.60", quantity: this.state.quantity},
-      { id: 3, name: "Jack Daniels", price: "3.35", quantity: this.state.quantity }
+      { id: 1, name: "Mountain Dew", price: "1.80" },
+      { id: 2, name: "Desperados", price: "2.60" },
+      { id: 3, name: "Jack Daniels", price: "3.35" }
     ];
 
     return (
       <div className="basket">
         <h1>Say Hi to your Basket... </h1>
       {itemList.map( item =>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <Item name={item.name} quantity={item.quantity} price={(item.price * item.quantity).toFixed(2)}/>
-            <input type='number' value={this.state.quantity}
-              onChange={this.handleUpdateQuantity} />
-          </form>
-        </div>
+          <Item name={item.name}  price={item.price}/>
       )}
-
       </div>
     )
   }
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,13 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class Item extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quantity: 0
+    };
+
+     this.handleUpdateQuantity = this.handleUpdateQuantity.bind(this);
+  }
+
+  handleUpdateQuantity(event){
+    this.setState({
+      quantity: Number(event.target.value)
+    });
+  }
 
   render () {
     return (
       <div className="item">
-      <ul>
-        <li>{this.props.name}  {this.props.quantity } sub-total: ${this.props.price} <a href="#" className="itemDelete">x</a></li>
-        </ul>
+        {this.props.name} price={(this.props.price * this.state.quantity).toFixed(2)}
+        <input type='number' value={this.state.quantity}
+          onChange={this.handleUpdateQuantity} />
+          <a href="#" className="itemDelete">x</a>
       </div>
     )
   }

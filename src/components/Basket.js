0734a13dd3fdc5fd3f702961756f7eeb0ef7d63a
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-25 11:31:44 +0100

    change in quantity updating sub-total for item

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,18 +2,41 @@ import React from 'react';
 import Item from './Item';
 
 class Basket extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quantity: 0
+    };
+
+    this.handleUpdateQuantity = this.handleUpdateQuantity.bind(this);
+  }
+
+  handleUpdateQuantity(event){
+    this.setState({
+      quantity: Number(event.target.value)
+    });
+  }
   render() {
     const itemList = [
-      { id: 1, name: "Mountain Dew", price: "1.80"},
-      { id: 2, name: "Desperados", price: "2.60"},
-      { id: 3, name: "Jack Daniels", price: "3.35"}
+      { id: 1, name: "Mountain Dew", price: "1.80", quantity: this.state.quantity },
+      { id: 2, name: "Desperados", price: "2.60", quantity: this.state.quantity},
+      { id: 3, name: "Jack Daniels", price: "3.35", quantity: this.state.quantity }
     ];
 
     return (
       <div className="basket">
-        {itemList.map( item =>
-          <Item name={item.name} price={item.price}/>
-        )}
+      {itemList.map( item =>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <Item name={item.name} quantity={item.quantity} price={item.price * item.quantity}/>
+            <input type='number' value={this.state.quantity}
+              onChange={this.handleUpdateQuantity} />
+          </form>
+        </div>
+      )}
+
       </div>
     )
   }

e02bc9a0e90dbfdae71ce3d41fc918b841364a80
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-26 12:51:47 +0100

    clean file with eslint, remove unused methods

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,35 +2,37 @@ import React from 'react';
 import Item from './Item';
 
 class Basket extends React.Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
+		this.state = {
+			quantity: 0
+		};
+	}
 
-    this.state = {
-      quantity: 0
-    }
-  }
+	render() {
+		const itemList = [
+			{ id: 1, name: 'Mountain Dew', price: '1.80', quantity: this.state.quantity },
+			{ id: 2, name: 'Desperados', price: '2.60', quantity: this.state.quantity  },
+			{ id: 3, name: 'Jack Daniels', price: '3.35' , quantity: this.state.quantity }
+		];
 
-  render() {
-    const itemList = [
-      { id: 1, name: "Mountain Dew", price: "1.80", quantity: this.state.quantity },
-      { id: 2, name: "Desperados", price: "2.60", quantity: this.state.quantity  },
-      { id: 3, name: "Jack Daniels", price: "3.35" , quantity: this.state.quantity }
-    ]
-
-    return (
-      <div className="basket">
-        <h1>Say Hi to your Basket... </h1>
-        <ul>
-          {itemList.map( item =>
-            <li key={item.id}>  <Item
-            name={item.name}
-            price={item.price}
-            quantity={item.quantity}/></li>
-          )}
-        </ul>
-      </div>
-    )
-  }
+		return (
+			<div className='basket'>
+				<h1>Your Drinks Basket... </h1>
+				<ul>
+					{itemList.map( item =>
+						<li key={item.id}>  <Item
+							name={item.name}
+							price={item.price}
+							quantity={item.quantity}
+						/>
+						</li>
+					)}
+				</ul>
+				<a href='#' className='itemDelete' onClick={this.resetAllQuantities}>Clear all</a>
+			</div>
+		);
+	}
 }
 
 export default Basket;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,40 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
 
 class Item extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		this.state = {
+			quantity: 0,
+			subTotal: 0
+		};
+		this._updateQuantity = this._updateQuantity.bind(this);
+		this._resetQuantity = this._resetQuantity.bind(this);
+	}
+	_updateQuantity(event){
+		this.setState({
+			quantity: Number(event.target.value),
+			subTotal: (this.props.price * this.state.quantity)
+		});
+	}
 
-    this.state = {
-      quantity: this.props.quantity
-    };
+	_resetQuantity(event){
+		this.setState({
+			quantity: 0
+		});
+	}
 
-    this.updateQuantity = this.updateQuantity.bind(this);
-    this.resetQuantity = this.resetQuantity.bind(this);
-  }
-
-  updateQuantity = event => {
-    this.setState({
-      quantity: Number(event.target.value)
-    });
-
-  }
-
-  resetQuantity = event => {
-    this.setState({
-      quantity: 0
-    });
-  };
-
-  render () {
-    return (
-      <div className="item">
-        {this.props.name} price={(this.props.price * this.state.quantity).toFixed(2)}
-        <input type='number' className='quantity' value={this.state.quantity}
-          onChange={this.updateQuantity} />
-          <a href="#" className="itemDelete" onClick={this.resetQuantity}>x</a>
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div className="item">
+				<div className='name'>{this.props.name}</div>
+				<div><input type='number' className='quantity' onChange={this._updateQuantity} value={this.state.quantity}/></div>
+				<div className='sub-total'>${(this.props.price * this.state.quantity).toFixed(2)}</div>
+				<div><a href="#" className="itemDelete" onClick={this._resetQuantity}>x</a></div>
+			</div>
+		);
+	}
 }
 export default Item;
